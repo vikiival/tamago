319fafda4170c493de9db5a1bbc0bc1487443590
Viki Val <viktorko99@gmail.com>
2022-06-10 20:02:22 +0200

    :test_tube: test for the playable contract

---
+++ server/test/play.ts
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Playable", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Playable = await ethers.getContractFactory("Playable");
+    const greeter = await Playable.deploy();
+    await greeter.deployed();
+
+    expect(await greeter.isDayAfterWatering(0)).to.equal(false);
+  });
+});
