fd2f5ab3ec99417d51f093e4aa7a0d8e74bce065
Viki Val <viktorko99@gmail.com>
2022-06-11 13:31:13 +0200

    :tada: pages for plant and water

---
+++ web/src/pages/plant.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import CenteredCard from '@/components/Card/CenteredCard'
+
+const Plant: FC = () => {
+	return (
+		<CenteredCard>
+			Plant
+		</CenteredCard>
+
+	)
+}
+
+export default Plant
---
+++ web/src/pages/water.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import CenteredCard from '@/components/Card/CenteredCard'
+
+const Water: FC = () => {
+	return (
+		<CenteredCard>
+			Water
+		</CenteredCard>
+
+	)
+}
+
+export default Water
