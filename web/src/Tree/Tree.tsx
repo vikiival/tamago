ac539c1f39010340c5f88de6355e004fa1b76490
FrankBevr <frank_dierolf@web.de>
2022-06-12 08:53:28 +0200

    :zap: Tree renders according to level

--- web/src/Tree/Tree.tsx
+++ web/src/Tree/Tree.tsx
@@ -7,6 +7,7 @@ const Sketch = dynamic(() => import('react-p5').then(mod => mod.default), {
 })
 import p5Types from 'p5' //Import this for typechecking and intellisense
 import { TreeStatus } from '@/types'
+import { ScaleIcon } from '@heroicons/react/outline'
 
 const Tree: FC<TreeStatus> = ({ level }) => {
 	console.log('called tree')
@@ -17,83 +18,582 @@ const Tree: FC<TreeStatus> = ({ level }) => {
 	}
 
 	const draw = (p5: p5Types) => {
-		p5.background(100)
-		p5.translate(p5.width / 4, p5.height / 4 + 300)
-		const branch = len => {
-			p5.push()
-			if (len > 10) {
-				p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
-				p5.stroke(70, 40, 20)
-				p5.line(0, 0, 0, -len)
-				p5.translate(0, -len)
-				p5.rotate(p5.random(-20, -30))
-				branch(len * p5.random(0.7, 0.9))
-				p5.rotate(p5.random(50, 60))
-				branch(len * p5.random(0.7, 0.9))
-			} else {
-				var r = 80 + p5.random(-20, 20)
-				var g = 120 + r
-				var b = 40 + p5.random(-20, 20)
-				p5.fill(r, g, b, 150)
-				p5.noStroke()
-				// ellipse(0, 0, 10)
-				p5.beginShape()
-				for (var i = 45; i < 135; i++) {
-					var rad = 15
-					var x = rad * p5.cos(i)
-					var y = rad * p5.sin(i)
-					p5.vertex(x, y)
+		level = 13
+		if (level === 1) {
+			p5.background(255)
+			p5.translate(p5.width / 2, p5.height / 2 + 50)
+			const branch = len => {
+				p5.push()
+				if (len > 15 - level) {
+					p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+					p5.stroke(70, 40, 20)
+					p5.line(0, 0, 0, -len)
+					p5.translate(0, -len)
+					// p5.scale(1,1.1)
+					p5.rotate(p5.random(-20, -30))
+					branch(len * p5.random(0.7, 0.9))
+					p5.rotate(p5.random(50, 60))
+					branch(len * p5.random(0.7, 0.9))
+				} else {
+					var r = 80 + p5.random(-20, 20)
+					var g = 120 + r
+					var b = 40 + p5.random(-20, 20)
+					p5.fill(r, g, b, 150)
+					p5.noStroke()
+					// ellipse(0, 0, 10)
+					// p5.beginShape()
+					// for (var i = 45; i < 135; i++) {
+					// 	var rad = 15
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(i)
+					// 	p5.vertex(x, y)
+					// }
+					// for (var i = 135; i > 40; i--) {
+					// 	var rad = 15
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(-i) + 20
+					// 	p5.vertex(x, y)
+					// }
+					// p5.endShape(p5.CLOSE)
 				}
-				for (var i = 135; i > 40; i--) {
-					var rad = 15
-					var x = rad * p5.cos(i)
-					var y = rad * p5.sin(-i) + 20
-					p5.vertex(x, y)
-				}
-				p5.endShape(p5.CLOSE)
+				p5.pop()
 			}
-			p5.pop()
+			branch(20 * 1) // TODO: @Frank pls change
 		}
-		branch(20 * level) // TODO: @Frank pls change
+		if (level === 2) {
+			p5.background(255)
+			p5.translate(p5.width / 2, p5.height / 2 + 50)
+			const branch = len => {
+				p5.push()
+				if (len > 15 - level) {
+					p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+					p5.stroke(70, 40, 20)
+					p5.line(0, 0, 0, -len)
+					p5.translate(0, -len)
+					// p5.scale(1,1.1)
+					p5.rotate(p5.random(-20, -30))
+					branch(len * p5.random(0.7, 0.9))
+					p5.rotate(p5.random(50, 60))
+					branch(len * p5.random(0.7, 0.9))
+				} else {
+					var r = 80 + p5.random(-20, 20)
+					var g = 120 + r
+					var b = 40 + p5.random(-20, 20)
+					p5.fill(r, g, b, 150)
+					p5.noStroke()
+					// ellipse(0, 0, 10)
+					// p5.beginShape()
+					// for (var i = 45; i < 135; i++) {
+					// 	var rad = 15
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(i)
+					// 	p5.vertex(x, y)
+					// }
+					// for (var i = 135; i > 40; i--) {
+					// 	var rad = 15
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(-i) + 20
+					// 	p5.vertex(x, y)
+					// }
+					// p5.endShape(p5.CLOSE)
+				}
+				p5.pop()
+			}
+			branch(20 * 1) // TODO: @Frank pls change
+		}
+		if (level === 3) {
+			p5.background(255)
+			p5.translate(p5.width / 2, p5.height / 2 + 50)
+			const branch = len => {
+				p5.push()
+				if (len > 15 - level) {
+					p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+					p5.stroke(70, 40, 20)
+					p5.line(0, 0, 0, -len)
+					p5.translate(0, -len)
+					// p5.scale(1,1.1)
+					p5.rotate(p5.random(-20, -30))
+					branch(len * p5.random(0.7, 0.9))
+					p5.rotate(p5.random(50, 60))
+					branch(len * p5.random(0.7, 0.9))
+				} else {
+					var r = 80 + p5.random(-20, 20)
+					var g = 120 + r
+					var b = 40 + p5.random(-20, 20)
+					p5.fill(r, g, b, 150)
+					p5.noStroke()
+					// ellipse(0, 0, 10)
+					// p5.beginShape()
+					// for (var i = 45; i < 135; i++) {
+					// 	var rad = 15
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(i)
+					// 	p5.vertex(x, y)
+					// }
+					// for (var i = 135; i > 40; i--) {
+					// 	var rad = 15
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(-i) + 20
+					// 	p5.vertex(x, y)
+					// }
+					// p5.endShape(p5.CLOSE)
+				}
+				p5.pop()
+			}
+			branch(20 * 1) // TODO: @Frank pls change
+		}
+		if (level === 4) {
+			p5.background(255)
+			p5.translate(p5.width / 2, p5.height / 2 + 50)
+			const branch = len => {
+				p5.push()
+				if (len > 15 - level) {
+					p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+					p5.stroke(70, 40, 20)
+					p5.line(0, 0, 0, -len)
+					p5.translate(0, -len)
+					// p5.scale(1,1.1)
+					p5.rotate(p5.random(-20, -30))
+					branch(len * p5.random(0.7, 0.9))
+					p5.rotate(p5.random(50, 60))
+					branch(len * p5.random(0.7, 0.9))
+				} else {
+					var r = 80 + p5.random(-20, 20)
+					var g = 120 + r
+					var b = 40 + p5.random(-20, 20)
+					p5.fill(r, g, b, 150)
+					p5.noStroke()
+					// ellipse(0, 0, 10)
+					// p5.beginShape()
+					// for (var i = 45; i < 135; i++) {
+					// 	var rad = 15
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(i)
+					// 	p5.vertex(x, y)
+					// }
+					// for (var i = 135; i > 40; i--) {
+					// 	var rad = 15
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(-i) + 20
+					// 	p5.vertex(x, y)
+					// }
+					// p5.endShape(p5.CLOSE)
+				}
+				p5.pop()
+			}
+			branch(20 * 1) // TODO: @Frank pls change
+		}
+		if (level === 5) {
+			p5.background(255)
+			p5.translate(p5.width / 2, p5.height / 2 + 50)
+			const branch = len => {
+				p5.push()
+				if (len > 15 - level) {
+					p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+					p5.stroke(70, 40, 20)
+					p5.line(0, 0, 0, -len)
+					p5.translate(0, -len)
+					// p5.scale(1,1.1)
+					p5.rotate(p5.random(-20, -30))
+					branch(len * p5.random(0.7, 0.9))
+					p5.rotate(p5.random(50, 60))
+					branch(len * p5.random(0.7, 0.9))
+				} else {
+					var r = 80 + p5.random(-20, 20)
+					var g = 120 + r
+					var b = 40 + p5.random(-20, 20)
+					p5.fill(r, g, b, 150)
+					p5.noStroke()
+					// ellipse(0, 0, 10)
+					// p5.beginShape()
+					// for (var i = 45; i < 135; i++) {
+					// 	var rad = 15
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(i)
+					// 	p5.vertex(x, y)
+					// }
+					// for (var i = 135; i > 40; i--) {
+					// 	var rad = 15
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(-i) + 20
+					// 	p5.vertex(x, y)
+					// }
+					// p5.endShape(p5.CLOSE)
+				}
+				p5.pop()
+			}
+			branch(20 * 1) // TODO: @Frank pls change
+		}
+		if (level === 6) {
+			p5.background(255)
+			p5.translate(p5.width / 2, p5.height / 2 + 50)
+			const branch = len => {
+				p5.push()
+				if (len > 15 - level) {
+					p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+					p5.stroke(70, 40, 20)
+					p5.line(0, 0, 0, -len)
+					p5.translate(0, -len)
+					// p5.scale(1,1.1)
+					p5.rotate(p5.random(-20, -30))
+					branch(len * p5.random(0.7, 0.9))
+					p5.rotate(p5.random(50, 60))
+					branch(len * p5.random(0.7, 0.9))
+				} else {
+					var r = 80 + p5.random(-20, 20)
+					var g = 120 + r
+					var b = 40 + p5.random(-20, 20)
+					p5.fill(r, g, b, 150)
+					p5.noStroke()
+					// ellipse(0, 0, 10)
+					// p5.beginShape()
+					// for (var i = 45; i < 135; i++) {
+					// 	var rad = 15
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(i)
+					// 	p5.vertex(x, y)
+					// }
+					// for (var i = 135; i > 40; i--) {
+					// 	var rad = 15
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(-i) + 20
+					// 	p5.vertex(x, y)
+					// }
+					// p5.endShape(p5.CLOSE)
+				}
+				p5.pop()
+			}
+			branch(20 * 1) // TODO: @Frank pls change
+		}
+		if (level === 7) {
+			p5.background(255)
+			p5.translate(p5.width / 2, p5.height / 2 + 50)
+			const branch = len => {
+				p5.push()
+				if (len > 15 - level) {
+					p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+					p5.stroke(70, 40, 20)
+					p5.line(0, 0, 0, -len)
+					p5.translate(0, -len)
+					// p5.scale(1,1.1)
+					p5.rotate(p5.random(-20, -30))
+					branch(len * p5.random(0.7, 0.9))
+					p5.rotate(p5.random(50, 60))
+					branch(len * p5.random(0.7, 0.9))
+				} else {
+					var r = 80 + p5.random(-20, 20)
+					var g = 120 + r
+					var b = 40 + p5.random(-20, 20)
+					p5.fill(r, g, b, 150)
+					p5.noStroke()
+					// ellipse(0, 0, 10)
+					// p5.beginShape()
+					// for (var i = 45; i < 135; i++) {
+					// 	var rad = 15
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(i)
+					// 	p5.vertex(x, y)
+					// }
+					// for (var i = 135; i > 40; i--) {
+					// 	var rad = 15
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(-i) + 20
+					// 	p5.vertex(x, y)
+					// }
+					// p5.endShape(p5.CLOSE)
+				}
+				p5.pop()
+			}
+			branch(20 * 1) // TODO: @Frank pls change
+		}
+		if (level === 8) {
+			p5.background(255)
+			p5.translate(p5.width / 2, p5.height / 2 + 50)
+			const branch = len => {
+				p5.push()
+				if (len > 15 - level) {
+					p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+					p5.stroke(70, 40, 20)
+					p5.line(0, 0, 0, -len)
+					p5.translate(0, -len)
+					// p5.scale(1,1.1)
+					p5.rotate(p5.random(-20, -30))
+					branch(len * p5.random(0.7, 0.9))
+					p5.rotate(p5.random(50, 60))
+					branch(len * p5.random(0.7, 0.9))
+				} else {
+					var r = 80 + p5.random(-20, 20)
+					var g = 120 + r
+					var b = 40 + p5.random(-20, 20)
+					p5.fill(r, g, b, 150)
+					p5.noStroke()
+					// ellipse(0, 0, 10)
+					p5.beginShape()
+					for (var i = 45; i < 135; i++) {
+						var rad = 3
+						var x = rad * p5.cos(i)
+						var y = rad * p5.sin(i)
+						p5.vertex(x, y)
+					}
+					// for (var i = 135; i > 40; i--) {
+					// 	var rad = 8
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(-i) + 20
+					// 	p5.vertex(x, y)
+					// }
+					p5.endShape(p5.CLOSE)
+				}
+				p5.pop()
+			}
+			branch(20 * 1) // TODO: @Frank pls change
+		}
+		if (level === 9) {
+			p5.background(255)
+			p5.translate(p5.width / 2, p5.height / 2 + 50)
+			const branch = len => {
+				p5.push()
+				if (len > 15 - level) {
+					p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+					p5.stroke(70, 40, 20)
+					p5.line(0, 0, 0, -len)
+					p5.translate(0, -len)
+					// p5.scale(1,1.1)
+					p5.rotate(p5.random(-20, -30))
+					branch(len * p5.random(0.7, 0.9))
+					p5.rotate(p5.random(50, 60))
+					branch(len * p5.random(0.7, 0.9))
+				} else {
+					var r = 80 + p5.random(-20, 20)
+					var g = 120 + r
+					var b = 40 + p5.random(-20, 20)
+					p5.fill(r, g, b, 150)
+					p5.noStroke()
+					// ellipse(0, 0, 10)
+					p5.beginShape()
+					for (var i = 45; i < 135; i++) {
+						var rad = 5
+						var x = rad * p5.cos(i)
+						var y = rad * p5.sin(i)
+						p5.vertex(x, y)
+					}
+					// for (var i = 135; i > 40; i--) {
+					// 	var rad = 8
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(-i) + 20
+					// 	p5.vertex(x, y)
+					// }
+					p5.endShape(p5.CLOSE)
+				}
+				p5.pop()
+			}
+			branch(20 * 1) // TODO: @Frank pls change
+		}
+		if (level === 10) {
+			p5.background(255)
+			p5.translate(p5.width / 2, p5.height / 2 + 50)
+			const branch = len => {
+				p5.push()
+				if (len > 15 - level) {
+					p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+					p5.stroke(70, 40, 20)
+					p5.line(0, 0, 0, -len)
+					p5.translate(0, -len)
+					// p5.scale(1,1.1)
+					p5.rotate(p5.random(-20, -30))
+					branch(len * p5.random(0.7, 0.9))
+					p5.rotate(p5.random(50, 60))
+					branch(len * p5.random(0.7, 0.9))
+				} else {
+					var r = 80 + p5.random(-20, 20)
+					var g = 120 + r
+					var b = 40 + p5.random(-20, 20)
+					p5.fill(r, g, b, 150)
+					p5.noStroke()
+					// ellipse(0, 0, 10)
+					p5.beginShape()
+					for (var i = 45; i < 135; i++) {
+						var rad = 8
+						var x = rad * p5.cos(i)
+						var y = rad * p5.sin(i)
+						p5.vertex(x, y)
+					}
+					// for (var i = 135; i > 40; i--) {
+					// 	var rad = 8
+					// 	var x = rad * p5.cos(i)
+					// 	var y = rad * p5.sin(-i) + 20
+					// 	p5.vertex(x, y)
+					// }
+					p5.endShape(p5.CLOSE)
+				}
+				p5.pop()
+			}
+			branch(20 * 1) // TODO: @Frank pls change
+		}
+		if (level === 11) {
+			p5.background(255)
+			p5.translate(p5.width / 2, p5.height / 2 + 50)
+			const branch = len => {
+				p5.push()
+				if (len > 15 - level) {
+					p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+					p5.stroke(70, 40, 20)
+					p5.line(0, 0, 0, -len)
+					p5.translate(0, -len)
+					// p5.scale(1,1.1)
+					p5.rotate(p5.random(-20, -30))
+					branch(len * p5.random(0.7, 0.9))
+					p5.rotate(p5.random(50, 60))
+					branch(len * p5.random(0.7, 0.9))
+				} else {
+					var r = 80 + p5.random(-20, 20)
+					var g = 120 + r
+					var b = 40 + p5.random(-20, 20)
+					p5.fill(r, g, b, 150)
+					p5.noStroke()
+					// ellipse(0, 0, 10)
+					p5.beginShape()
+					for (var i = 45; i < 135; i++) {
+						var rad = 8
+						var x = rad * p5.cos(i)
+						var y = rad * p5.sin(i)
+						p5.vertex(x, y)
+					}
+					for (var i = 135; i > 40; i--) {
+						var rad = 8
+						var x = rad * p5.cos(i)
+						var y = rad * p5.sin(-i) + 20
+						p5.vertex(x, y)
+					}
+					p5.endShape(p5.CLOSE)
+				}
+				p5.pop()
+			}
+			branch(20 * 1) // TODO: @Frank pls change
+		}
+		if (level === 12) {
+			p5.background(255)
+			p5.translate(p5.width / 2, p5.height / 2 + 50)
+			const branch = len => {
+				p5.push()
+				if (len > 15 - level) {
+					p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+					p5.stroke(70, 40, 20)
+					p5.line(0, 0, 0, -len)
+					p5.translate(0, -len)
+					// p5.scale(1,1.1)
+					p5.rotate(p5.random(-20, -30))
+					branch(len * p5.random(0.7, 0.9))
+					p5.rotate(p5.random(50, 60))
+					branch(len * p5.random(0.7, 0.9))
+				} else {
+					var r = 80 + p5.random(-20, 20)
+					var g = 120 + r
+					var b = 40 + p5.random(-20, 20)
+					p5.fill(r, g, b, 150)
+					p5.noStroke()
+					// ellipse(0, 0, 10)
+					p5.beginShape()
+					for (var i = 45; i < 135; i++) {
+						var rad = 8
+						var x = rad * p5.cos(i)
+						var y = rad * p5.sin(i)
+						p5.vertex(x, y)
+					}
+					for (var i = 135; i > 40; i--) {
+						var rad = 8
+						var x = rad * p5.cos(i)
+						var y = rad * p5.sin(-i) + 20
+						p5.vertex(x, y)
+					}
+					p5.endShape(p5.CLOSE)
+				}
+				p5.pop()
+			}
+			branch(20 * 1) // TODO: @Frank pls change
+		}
+		if (level === 13) {
+			p5.background(255)
+			p5.translate(p5.width / 2, p5.height / 2 + 50)
+			const branch = len => {
+				p5.push()
+				if (len > 15 - level) {
+					p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+					p5.stroke(70, 40, 20)
+					p5.line(0, 0, 0, -len)
+					p5.translate(0, -len)
+					// p5.scale(1,1.1)
+					p5.rotate(p5.random(-20, -30))
+					branch(len * p5.random(0.7, 0.9))
+					p5.rotate(p5.random(50, 60))
+					branch(len * p5.random(0.7, 0.9))
+				} else {
+					var r = 80 + p5.random(-20, 20)
+					var g = 120 + r
+					var b = 40 + p5.random(-20, 20)
+					p5.fill(r, g, b, 150)
+					p5.noStroke()
+					// ellipse(0, 0, 10)
+					p5.beginShape()
+					for (var i = 45; i < 135; i++) {
+						var rad = 10
+						var x = rad * p5.cos(i)
+						var y = rad * p5.sin(i)
+						p5.vertex(x, y)
+					}
+					for (var i = 135; i > 40; i--) {
+						var rad = 10
+						var x = rad * p5.cos(i)
+						var y = rad * p5.sin(-i) + 20
+						p5.vertex(x, y)
+					}
+					p5.endShape(p5.CLOSE)
+				}
+				p5.pop()
+			}
+			branch(20 * 1) // TODO: @Frank pls change
+		}
+		if (level === 14) {
+			p5.background(255)
+			p5.translate(p5.width / 2, p5.height / 2 + 50)
+			const branch = len => {
+				p5.push()
+				if (len > 15 - level) {
+					p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+					p5.stroke(70, 40, 20)
+					p5.line(0, 0, 0, -len)
+					p5.translate(0, -len)
+					// p5.scale(1,1.1)
+					p5.rotate(p5.random(-20, -30))
+					branch(len * p5.random(0.7, 0.9))
+					p5.rotate(p5.random(50, 60))
+					branch(len * p5.random(0.7, 0.9))
+				} else {
+					var r = 80 + p5.random(-20, 20)
+					var g = 120 + r
+					var b = 40 + p5.random(-20, 20)
+					p5.fill(r, g, b, 15)
+					p5.noStroke()
+					// ellipse(0, 0, 10)
+					p5.beginShape()
+					for (var i = 45; i < 135; i++) {
+						var rad = 1
+						var x = rad * p5.cos(i)
+						var y = rad * p5.sin(i)
+						p5.vertex(x, y)
+					}
+					for (var i = 135; i > 40; i--) {
+						var rad = 1
+						var x = rad * p5.cos(i)
+						var y = rad * p5.sin(-i) + 20
+						p5.vertex(x, y)
+					}
+					p5.endShape(p5.CLOSE)
+				}
+				p5.pop()
+			}
+			branch(20 * 1) // TODO: @Frank pls change
+		}
 	}
-	//-----------------------///
-	//----PlaygroundFrank----///
-	//-----------------------///
-	// const branch = (len, p5: p5Types) => {
-	// 	p5.push()
-	// 	if (len > 10) {
-	// 		p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
-	// 		p5.stroke(70, 40, 20)
-	// 		p5.line(0, 0, 0, -len)
-	// 		p5.translate(0, -len)
-	// 		p5.rotate(p5.random(-20, -30))
-	// 		branch(len * p5.random(0.7, 0.9))
-	// 		p5.rotate(p5.random(50, 60))
-	// 		branch(len * p5.random(0.7, 0.9))
-	// 	} else {
-	// 		var r = 80 + p5.random(-20, 20)
-	// 		var g = 120 + r
-	// 		var b = 40 + p5.random(-20, 20)
-	// 		p5.fill(r, g, b, 150)
-	// 		p5.noStroke()
-	// 		// ellipse(0, 0, 10)
-	// 		p5.beginShape()
-	// 		for (var i = 45; i < 135; i++) {
-	// 			var rad = 15
-	// 			var x = rad * p5.cos(i)
-	// 			var y = rad * p5.sin(i)
-	// 			p5.vertex(x, y)
-	// 		}
-	// 		for (var i = 135; i > 40; i--) {
-	// 			var rad = 15
-	// 			var x = rad * p5.cos(i)
-	// 			var y = rad * p5.sin(-i) + 20
-	// 			p5.vertex(x, y)
-	// 		}
-	// 		p5.endShape(p5.CLOSE)
-	// 	}
-	// 	p5.pop()
-	// }
 
 	return <Sketch setup={setup} draw={draw} />
 }
