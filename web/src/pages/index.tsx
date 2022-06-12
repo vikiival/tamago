0bae5d8ec154ef6a7ee68c5222cd94d45bb6ebbd
Viki Val <viktorko99@gmail.com>
2022-06-12 13:17:14 +0200

    :art: added demo to landing page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,17 @@
-import type { NextPage } from 'next'
-import { APP_NAME } from '@/lib/consts'
-import { BookOpenIcon, CodeIcon, ShareIcon } from '@heroicons/react/outline'
+
 import CenteredCard from '@/components/Card/CenteredCard'
+import MintButton from '@/components/Button/MintButton'
+import type { NextPage } from 'next'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
 	return (
 		<CenteredCard>
-			ahoj
+			<div className="flex flex-col">
+				<h5 className="font-medium leading-tight text-xl p-1 text-blue-600">Plant your Tamagochi Tree</h5>
+				<Image src="/images/movie.gif" alt="me" width="512" height="512" />
+				{/* <MintButton /> */}
+			</div>
 		</CenteredCard>
 
 	)
