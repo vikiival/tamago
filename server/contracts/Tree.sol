a6e2a3ad1b948faec1f9fde0e6e9dbfced01644c
Viki Val <viktorko99@gmail.com>
2022-06-12 10:37:41 +0200

    :zap: maps from contract id to token id

--- server/contracts/Tree.sol
+++ server/contracts/Tree.sol
@@ -10,6 +10,7 @@ contract Tree is ERC721, Playable {
     event Planted(address indexed from, uint256 indexed id);
     using Counters for Counters.Counter;
     Counters.Counter public _tokenCounter;
+    mapping(address => uint256) private _treeOwner;
 
     constructor() ERC721("TamagoTree", "TREE") {
       console.log("Deploying a Tree with greeting:");
@@ -20,11 +21,20 @@ contract Tree is ERC721, Playable {
         uint256 current = _tokenCounter.current();
         _mint(msg.sender, current);
         _water(current);
+        _setOwnerToTree(msg.sender, current);
         _tokenCounter.increment();
     }
 
     function water(uint256 _tokenId) external {
         _water(_tokenId);
+    }
+
+    function _setOwnerToTree(address _owner, uint256) internal {
+        _treeOwner[_owner] = _tokenId;
+    }
+
+    function getTreeIdByAccount(address _owner) public view returns (uint256) {
+        return _treeOwner[_owner];
     }
 
     modifier onlyOnePerUser() { 
