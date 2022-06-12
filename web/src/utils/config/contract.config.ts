4980000284d88f79f7f4c180f5712f03f24cbb65
Viki Val <viktorko99@gmail.com>
2022-06-12 11:09:00 +0200

    :memo: redeployed contract

--- web/src/utils/config/contract.config.ts
+++ web/src/utils/config/contract.config.ts
@@ -2,7 +2,7 @@ import { WriteContractArgs } from '@wagmi/core';
 import contractInterface from '../../../../server/artifacts/contracts/Tree.sol/Tree.json';
 
 const config: WriteContractArgs = {
-  addressOrName: '0x56157c71414aFCd499035092BFC1c96467c525eC', //process.env.NEXT_PUBLIC_CONTRACT,
+  addressOrName: '0xb67E87de592B950E27953586a2Abab08786dda08', //process.env.NEXT_PUBLIC_CONTRACT,
   contractInterface: contractInterface.abi,
 }
 console.log(process.env.NEXT_PUBLIC_CONTRACT)
