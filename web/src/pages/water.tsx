eea67bac3145e82159d90299c40267c0d3c101f7
Viki Val <viktorko99@gmail.com>
2022-06-12 11:08:45 +0200

    :zap: get all props from contract

--- web/src/pages/water.tsx
+++ web/src/pages/water.tsx
@@ -4,28 +4,47 @@ import Tree from '@/Tree/Tree'
 import WaterButton from '@/components/Button/WaterButton'
 import { useAccount, useConnect, useContractRead } from 'wagmi'
 import contractConfig from '@/utils/config/contract.config'
+const SECONDS_PER_DAY = 24 * 60 * 60
+const today = (() => {
+	const unix = Date.now() / 1000
+	const secs = unix % SECONDS_PER_DAY
+	return unix - secs
+})()
 
 const Water: NextPage = () => {
 	const { isConnected } = useConnect();
   const { data: account } = useAccount();
-	const { data: id } = useContractRead(
+	const { data: mayId } = useContractRead(
     contractConfig,
     'getTreeIdByAccount',
     { watch: true, args: account?.address, enabled: isConnected }
   );
 
-	const { data: level } = useContractRead(
+	
+	const id = mayId?.toNumber()
+	const enabled = isConnected && id !== undefined
+
+	const { data: mayLevel } = useContractRead(
     contractConfig,
     'getLevel',
-    { watch: true, args: account?.address, enabled: isConnected }
+    { watch: true, args: [id], enabled}
   );
 
-	console.log('stuff', id, level)
+	const { data: mayPlanted } = useContractRead(
+    contractConfig,
+    'getLastPlanted',
+    { watch: true, args: [id], enabled }
+  );
+
+	const level = mayLevel?.toNumber()
+	const planted = mayPlanted?.toNumber()
+
+	console.log('stuff', id, level, planted, today, planted === today)
 
 	return (
 		<CenteredCard>
-			<Tree level={4} lastWatered={5}/>
-			{id && <WaterButton id={id} />}
+			<Tree level={level} lastWatered={5}/>
+			{id !== undefined && <WaterButton id={id} alreadyWatered={planted === today} level={level} />}
 		</CenteredCard>
 
 	)
